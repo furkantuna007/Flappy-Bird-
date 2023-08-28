968ffcaa129f19276bf07411c2a0c6c7dec2144f
Furkan TUNA <72814790+furkantuna007@users.noreply.github.com>
2023-08-28 16:33:29 +0300

    Add files via upload

---
+++ GameCamera.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCamera : MonoBehaviour
+{
+    public GameObject target;
+    public Vector2 offset;
+
+    private Rigidbody2D cameraRigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraRigidbody = GetComponent<Rigidbody2D>();
+        
+        transform.position = new Vector3(target.transform.position.x + offset.x,
+        target.transform.position.y + offset.y,
+        transform.position.z);
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        cameraRigidbody.velocity = new Vector2(target.GetComponent<Rigidbody2D>().velocity.x, 0);
+        
+    }
+}

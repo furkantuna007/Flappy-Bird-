ecaa828adab34681469a411f1ae4450745b9aeb9
Furkan TUNA <72814790+furkantuna007@users.noreply.github.com>
2023-08-28 16:33:45 +0300

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float jumpingSpeed = 5f;
+    public float movingSpeed = 2f;
+    public float verticalLimit;
+
+    
+    private Rigidbody2D playerRigidbody;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidbody = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetAxis("Fire1") == 1f) {
+            playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, jumpingSpeed);
+        }
+
+        //move horizontally
+        playerRigidbody.velocity = new Vector2(movingSpeed, playerRigidbody.velocity.y);
+
+        //keep the player within bounds
+        if(transform.position.y > verticalLimit) {
+            transform.position = new Vector3(transform.position.x, verticalLimit, transform.position.z);
+            playerRigidbody .velocity = new Vector2(playerRigidbody.velocity.x, 0);
+        } else if (transform.position.y < -verticalLimit) {
+            transform.position = new Vector3(transform.position.x, -verticalLimit, transform.position.z);
+            playerRigidbody .velocity = new Vector2(playerRigidbody.velocity.x, 0);
+        }
+    }
+}
